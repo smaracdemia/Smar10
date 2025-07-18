77ba84724e0c985bd971dfcdcbeb3aa9eca3ecfa
smaracdemia <yabetsakalu8@gmail.com>
2025-07-18 03:26:03 +0300

    Update main-3.py

--- main-3.py
+++ main-3.py
@@ -227,7 +227,3 @@ def upload_file():
 def requirements():
     return render_template('requirements.html')
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    debug = os.environ.get('DEBUG', 'False').lower() == 'true'
-    app.run(host='0.0.0.0', port=port, debug=debug)
